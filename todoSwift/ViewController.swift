1fbc8fbeefba71c6eef8db5bdf6c5fa082aaf02c
yoshifuji <t.yoshifuji@me.com>
2021-04-04 01:07:06 +0900

    Add UITableView settings

--- todoSwift/ViewController.swift
+++ todoSwift/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+
+    var todoList = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func addButtonAction(_ sender: Any) {
+    }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoList.count;
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell", for: indexPath);
+        let todoTitle = todoList[indexPath.row];
+        cell.textLabel?.text = todoTitle;
+        return cell;
+    }
 }
 
